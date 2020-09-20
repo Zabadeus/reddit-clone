c07fb25e9c55a89a2e33258c93eb0037a1974156
Zabadeus <m.b.disy_new-guy@hotmail.de>
2020-09-20 14:21:36 +0200

    first commit

---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,157 @@
+import { MyContext } from "src/types";
+import {
+    Arg,
+    Ctx,
+    Field,
+    InputType,
+    Mutation,
+    ObjectType,
+    Query,
+    Resolver,
+} from "type-graphql";
+import argon2 from "argon2";
+import { User } from "../entities/User";
+import { EntityManager } from "@mikro-orm/postgresql";
+
+@InputType()
+class UsernamePasswordInput {
+    @Field()
+    username: string;
+
+    @Field()
+    password: string;
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+
+    @Field(() => User, { nullable: true })
+    user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+    @Query(() => User, { nullable: true })
+    async me(@Ctx() { em, req }: MyContext) {
+        // you are not logged in
+        if (!req.session.userId) {
+            return null;
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId });
+        return user;
+    }
+    @Mutation(() => UserResponse)
+    async register(
+        @Arg("options") options: UsernamePasswordInput,
+        @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "length must be greater than 2",
+                    },
+                ],
+            };
+        }
+
+        if (options.password.length <= 3) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "length must be greater than 3",
+                    },
+                ],
+            };
+        }
+
+        const hashedPassword = await argon2.hash(options.password);
+        let user;
+        try {
+            const result = await (em as EntityManager)
+                .createQueryBuilder(User)
+                .getKnexQuery()
+                .insert({
+                    username: options.username,
+                    password: hashedPassword,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                })
+                .returning("*");
+            user = result[0];
+            await em.persistAndFlush(user);
+        } catch (err) {
+            // duplicate username error
+            // || err.detail.includes("already exists")
+            if (err.code === "23505") {
+                return {
+                    errors: [
+                        {
+                            field: "username",
+                            message: "username already taken",
+                        },
+                    ],
+                };
+            }
+        }
+
+        // store user id session
+        // this will set a cookie on the user
+        // keep them logged in
+        req.session.userId = user.id;
+
+        return { user };
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg("options") options: UsernamePasswordInput,
+        @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, {
+            username: options.username,
+        });
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "that username doesn't exist",
+                    },
+                ],
+            };
+        }
+
+        const validPassword = await argon2.verify(
+            user.password,
+            options.password
+        );
+        if (!validPassword) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "incorrect password",
+                    },
+                ],
+            };
+        }
+
+        req.session!.userId = user.id;
+
+        return { user };
+    }
+}
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from "../components/NavBar";
+
+const Index = () => (
+    <>
+        <NavBar />
+        <div>Hello World</div>
+    </>
+);
+export default Index;
